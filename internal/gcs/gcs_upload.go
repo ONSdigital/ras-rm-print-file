149a0ce38f03d1778894cc1185e125619fececed
jdfox <jfoxgithub@gmail.com>
2020-10-21 15:52:33 +0100

    full path for rd and wr

--- internal/gcs/gcs_upload.go
+++ internal/gcs/gcs_upload.go
@@ -47,16 +47,17 @@ func (u *GCSUpload) UploadFile(filename string, contents []byte) error {
 	bucket := viper.GetString("BUCKET_NAME")
 	prefix := viper.GetString("PREFIX_NAME")
 	path := prefix + filename
+	fullpath := bucket + path
 	log.WithField("filename", path).WithField("bucket", bucket).Info("uploading to bucket")
 
 	ctx, cancel := context.WithTimeout(u.ctx, time.Second*50)
 	defer cancel()
 
 	// GCSUpload an object with storage.Writer.
-	wc := u.client.Bucket(bucket).Object(path).NewWriter(ctx)
+	wc := u.client.Bucket(bucket).Object(fullpath).NewWriter(ctx)
 	log.WithField("filename", path).WithField("bucket", bucket).Info("about to write contents to bucket")
 	if _, err := wc.Write(contents); err != nil {
-		log.WithError(err).Error("error writing bytes to bucket")
+		log.WithError(err).Error("error writing bytes to bucket " + fullpath)
 		return err
 	}
 	if err := wc.Close(); err != nil {
